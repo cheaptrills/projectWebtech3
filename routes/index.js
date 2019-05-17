51a6d51da60220272e58d46ec926d1f8758ffde8
kinoodaphne <daphne.kinoo@outlook.be>
2019-05-17 14:56:24 +0200

    Created login/register/index file
    - Created routes for:
     - Login
     - Register
    - Created files and layout for:
     - Login
     - Register
     - Index (chat)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pug something' });
+  res.render('index', { title: 'Chatbot' });
+});
+
+/* GET login page. */
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET register page. */
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
